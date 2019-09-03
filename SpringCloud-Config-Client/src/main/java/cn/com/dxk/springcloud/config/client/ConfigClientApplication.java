cd503171126b312b6d60071436428e9d846fac9d
丁杏凯 <1760029958@163.com>
2019-09-03 22:57:53 +0800

    配置中心集成消息中间件kafka 动态刷新配置中心数据 2019年9月3日22:57:42 丁杏凯

--- SpringCloud-Config-Client/src/main/java/cn/com/dxk/springcloud/config/client/ConfigClientApplication.java
+++ SpringCloud-Config-Client/src/main/java/cn/com/dxk/springcloud/config/client/ConfigClientApplication.java
@@ -2,9 +2,11 @@ package cn.com.dxk.springcloud.config.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringCloudApplication
+@RefreshScope
 public class ConfigClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApplication.class,args);
--- SpringCloud-Config-Server/src/main/java/cn/com/dxk/springcloud/config/server/ConfigServerApplication.java
+++ SpringCloud-Config-Server/src/main/java/cn/com/dxk/springcloud/config/server/ConfigServerApplication.java
@@ -3,11 +3,35 @@ package cn.com.dxk.springcloud.config.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringCloudApplication
 @EnableConfigServer
+@RestController
 public class ConfigServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class,args);
+    }
+
+    /**
+     * 刷新
+     */
+    @RequestMapping(value = "refresh")
+    public void refreshConfig() {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders httpHeaders = new HttpHeaders();
+        //设置content_type为json要不然会报415的错误
+        httpHeaders.add(HttpHeaders.CONTENT_TYPE,"application/json");
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(null,httpHeaders);
+        //以post方法访问真正的刷新链接
+        ResponseEntity<String> stringResponseEntity = restTemplate.postForEntity("http://localhost:8084/actuator/bus-refresh",
+                request, String.class);
     }
 }
